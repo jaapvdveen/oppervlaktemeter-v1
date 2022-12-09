7bf6242bfe53936254401c811f4af461c3484bbb
jaapvdveen <jaapvdveen@users.noreply.github.com>
2022-12-09 13:54:17 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,22 @@
 input.onButtonPressed(Button.A, function () {
     lengte = grove.measureInCentimeters(DigitalPin.P0)
-    _4digit.show(lengte)
+    display.show(lengte)
 })
 input.onButtonPressed(Button.AB, function () {
     let afstand_y = 0
     let afstand_x = 0
     oppervlakte = afstand_x * afstand_y
-    _4digit.show(oppervlakte)
+    display.show(oppervlakte)
 })
 input.onButtonPressed(Button.B, function () {
     breedte = grove.measureInCentimeters(DigitalPin.P0)
-    _4digit.show(breedte)
+    display.show(breedte)
 })
-let _4digit: grove.TM1637 = null
+let display: grove.TM1637 = null
 let oppervlakte = 0
 let breedte = 0
 let lengte = 0
 lengte = 0
 breedte = 0
 oppervlakte = 0
-_4digit = grove.createDisplay(DigitalPin.P1, DigitalPin.P15)
+display = grove.createDisplay(DigitalPin.P1, DigitalPin.P15)
